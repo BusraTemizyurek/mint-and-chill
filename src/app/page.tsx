53d181aaa7dbbd0755233ad037293d26c2722c7c
Busra Temizyurek <busratemizyureek@gmail.com>
2025-01-30 13:58:47 -0800

    cursor glow is added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,6 +20,7 @@ import {
 } from "@coinbase/onchainkit/identity";
 import { useState } from "react";
 import axios from "axios";
+import CursorGlow from "../components/CursorGlow";
 
 export default function Home() {
   const { address, chain } = useAccount();
@@ -164,6 +165,7 @@ export default function Home() {
   };
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <CursorGlow />
       {/* Decorative Background */}
       <div className="absolute inset-0 -z-10 overflow-hidden">
         <svg
